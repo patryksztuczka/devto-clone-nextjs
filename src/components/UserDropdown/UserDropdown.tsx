5d3d9c73ba8dedb0a16607d5be2721f8f0136203
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2022-12-12 10:40:16 +0100

    Increased clickable space of button that navigate to profile

--- src/components/UserDropdown/UserDropdown.tsx
+++ src/components/UserDropdown/UserDropdown.tsx
@@ -45,9 +45,12 @@ const UserDropdown = () => {
       {isOpen && (
         <div className="absolute top-[52px] right-0 flex w-[calc(100vw-16px)] flex-col rounded-md bg-white p-2 shadow-dropdown lg:w-60">
           <div className="flex w-full border-b-[1px] border-b-borderGrey pb-2">
-            <span className="w-full cursor-pointer rounded-md px-4 py-2 font-medium hover:bg-lightBlue hover:text-blue hover:underline">
-              <Link href={`/${user?.username}`}>{session.user?.name}</Link>
-            </span>
+            <Link
+              href={`/${user?.username}`}
+              className="flex w-full cursor-pointer rounded-md px-4 py-2 font-medium hover:bg-lightBlue hover:text-blue hover:underline "
+            >
+              <span className="">{session.user?.name}</span>
+            </Link>
           </div>
           <div className="flex w-full flex-col border-b-[1px] border-b-borderGrey py-2">
             {userDropdownOptions.map((option) => (
